f63eb20c5ff12ab0dc90cd2d329060fca9b03245
Joshua David Crofts <croftjos@fit.cvut.cz>
2022-10-22 14:40:28 +0200

    hw02: added main.c

---
+++ hw02/main.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+bool isPrime(int in); //this is hell
+
+int main()
+{
+	int startInterval, endInterval, sum = 0, count = 0;
+	char sel;
+	printf ("Intervaly:\n");
+	
+	while (1)
+	{
+		if (scanf ("%c %d %d", &sel, &startInterval, &endInterval) == 3 && startInterval <= endInterval && startInterval >= 1)
+		{		
+			if (sel != '?' && sel != '#')
+			{
+				printf("Nespravny vstup.\n");
+				return EXIT_FAILURE;
+			}
+		
+			for (int i = startInterval; i <= endInterval; i++)
+			{
+				if (isPrime (i) == true) //exclude primes from testing
+					continue;
+					
+				for (int j = 1; j < i; j++) //sum proper divisors
+				{
+					if (i % j == 0)
+					{
+						sum += j;
+					}
+				}
+				
+				if (isPrime (sum) == true) //test whether sum is prime
+				{
+					printf("%d\n", i);
+					count++;
+				}
+				
+				sum = 0;
+			}
+		}
+		
+		else if (getchar() == EOF) //check for eof
+			return EXIT_SUCCESS;
+	}
+	
+	printf("Nespravny vstup\n");
+	return EXIT_FAILURE; 	
+}
+
+bool isPrime(int in) //determines whether a number is prime, returns bool
+{
+	for (int i = 2; i < in; i++)
+	{
+		if (in % i == 0)
+			return false;
+	}
+	
+	return true;
+}
