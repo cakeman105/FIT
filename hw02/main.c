b809457a8406668fa56139b690ed964610d42e20
Joshua David Crofts <croftjos@fit.cvut.cz>
2022-10-26 13:35:11 +0200

    hw02: changes to main.c

--- hw02/main.c
+++ hw02/main.c
@@ -2,8 +2,9 @@
 #include <stdlib.h>
 #include <math.h>
 
-bool isPrime(int in);
-int sumDivisors(int in);
+bool isPrime (int in);
+int sumDivisors (int in);
+
 int main()
 {
 	int startInterval, endInterval, sum = 0, count = 0;
@@ -52,13 +53,16 @@ int main()
 	}
 }
 
-bool isPrime(int in) //determines whether a number is prime, returns bool
+bool isPrime (int in) //determines whether a number is prime, returns bool
 {					 //needs improvement
-	if (in == 1|| in == 0)
+	if (in == 2)
 		return true;
+	
+	if (in % 2 == 0)
+		return false;
 		
-	for (int i = 2; i <= sqrt (in); i++) //run to sqrt (in) to reduce time
-	{
+	for (int i = 3; i <= sqrt (in); i += 2) //run to sqrt (in) to reduce time
+	{										//some O(n^1/2)
 		if (in % i == 0)
 			return false;
 	}
@@ -66,11 +70,13 @@ bool isPrime(int in) //determines whether a number is prime, returns bool
 	return true;
 }
 
-int sumDivisors(int in) //calculate the sum of all proper divisors of natural number
+int sumDivisors (int in) //calculate the sum of all proper divisors of natural number
 {
     int res = 0;
-    if(in == 1)
+    
+    if (in == 1)
       return res;
+      
     for (int i = 2; i <= sqrt (in); i++) //run to sqrt (in), ditto
     {
         if (in % i == 0)
@@ -83,4 +89,3 @@ int sumDivisors(int in) //calculate the sum of all proper divisors of natural nu
     }
     return (res + 1);
 }
-
