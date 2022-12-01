b3c0bf07548a522b912ed6729eaef43a31f3fb24
Joshua David Crofts <croftjos@fit.cvut.cz>
2022-12-02 00:08:53 +0100

    hw06: changes to main.c

--- hw06/main.c
+++ hw06/main.c
@@ -6,6 +6,11 @@
 #include <assert.h>
 #endif /* __PROGTEST__ */
 
+/**
+  * struct for storing substring index in replace[][2] and its location in initial text
+  * int word, int index
+  */
+  
 struct Instances
 {
 	int word;
@@ -15,11 +20,28 @@ struct Instances
 /**
   * check whether a word is a substring of another word in replace[][0]
   * @param[in] const char * (*replace)[2]
-  * @return 0 on success, 1 on failure
+  * @return 0 on success, -1 on failure
   */
   
 int checkList(const char * (*replace)[2])
 {
+	int i = 0, j = 0, sub = 0;
+	
+	const char * ptr;
+	while (replace[i][0] != NULL)
+	{
+		while (replace[j][0] != NULL)
+		{
+			if ((ptr = strstr(replace[j][0],replace[i][0])) && ptr == replace[j][0])
+				sub++;
+			if (sub >= 2)
+				return -1;
+			j++;
+		}
+		sub = 0;
+		j = 0;
+		i++;
+	}
 	return 0;
 }
 
@@ -29,6 +51,7 @@ int checkList(const char * (*replace)[2])
   * @param[in] const void * b
   * @return (instA->index > instB->index) - (instA->index < instB->index)
   */
+  
 int compare(const void * a, const void * b)
 {
 	const Instances * instA = (Instances *) a;
@@ -55,12 +78,15 @@ int findInstances(Instances ** arr, char * text, const char * (*replace)[2])
 		char * ptr = strstr(text, replace[i][0]);
 		while (ptr)
 		{
-			tmp[amount].word = i;
-			tmp[amount].index = 0 - (text - ptr);
-			tmp = (Instances *) realloc(*arr, (amount + 2) * sizeof(Instances));
-			amount++;
+			if (text[ptr - text])
+			{
+				tmp[amount].word = i;
+				tmp[amount].index = ptr - text; //pointer arithmetic
+				tmp = (Instances *) realloc(*arr, (amount + 2) * sizeof(Instances));
+				amount++;
+				*arr = tmp;
+			}
 			ptr = strstr(ptr + 1, replace[i][0]);
-			*arr = tmp;
 		}
 		
 		i++;
@@ -100,29 +126,44 @@ int calculatePost(char * text, Instances ** arr, const char * (*replace)[2], int
 
 char * replaceInstances(char * textInit, const char * (*replace)[2], Instances ** arr, int length, int size)
 {
-	char * textCpyOut = (char *) malloc(length * sizeof(char));
-	int startingPoint = 0;
-	int startingPoint2 = 0;
-	for (int i = 0; i < size; i++)
-	{
-		for (int j = startingPoint, k = startingPoint2; j < (*arr)[i].index; j++, k++)
-			textCpyOut[k] = textInit[j];
-			
-		strcat(textCpyOut, replace[(*arr)[i].word][1]);
-		startingPoint += strlen(replace[(*arr)[i].word][1]) + 1;
-		startingPoint2 += strlen(replace[(*arr)[i].word][0]);
-	}
-	printf("%s\n", textCpyOut);
+	char * textCpyOut = (char *) calloc(length + 1, sizeof(char));
+    int startingPointOut = 0;
+    int startingPointIn = 0;
+    for (int i = 0; i <= size; i++)
+    {
+    	if (i == size)
+    	{
+    		for (int j = startingPointOut, k = startingPointIn; textInit[k] != '\0'; j++, k++) //copy the rest of the string
+         	   textCpyOut[j] = textInit[k];
+         	break;
+    	}
+        for (int j = startingPointOut, k = startingPointIn; j < (*arr)[i].index + (startingPointOut - startingPointIn); j++, k++) //copy the chars before substring instance
+        {
+            textCpyOut[j] = textInit[k];
+            startingPointOut++;
+            startingPointIn++;
+        }
+        
+        strcat(textCpyOut, replace[(*arr)[i].word][1]); //copy the substring replacement
+        startingPointOut += strlen(replace[(*arr)[i].word][1]); 
+        startingPointIn += strlen(replace[(*arr)[i].word][0]); 
+    }
+   	textCpyOut[length] = '\0';
 	return textCpyOut;
 }
 
 /**
-  * 
+  * take a string and replace instances of substring with defined counterparts
+  * @param[in] const char * text
+  * @param[in] const char * (*replace)[2]
+  * @return edited string or NULL on error
   */
+
 char * newSpeak ( const char * text, const char * (*replace)[2])
 {	
-	if (checkList(replace) == 1)
+	if (checkList(replace) == -1) //check whether a string is a part 
 		return NULL;
+		
 	char * textCpy = (char *) malloc((strlen(text) + 1) * sizeof(char));
 	strcpy(textCpy, text);
 	Instances * arr = (Instances *) malloc (sizeof(Instances));
@@ -131,11 +172,12 @@ char * newSpeak ( const char * text, const char * (*replace)[2])
 	if (size == 0) //return the initial text if nothing is found
 		return textCpy;
 	
-	qsort(arr, size, sizeof(Instances), compare);
+	qsort(arr, size, sizeof(Instances), compare); //sort struct array to improve replacement
 	int newLen = calculatePost(textCpy, &arr, replace, size);
-	replaceInstances(textCpy, replace, &arr, newLen, size);
+	char * out = replaceInstances(textCpy, replace, &arr, newLen, size);
+	free(arr);
 	free(textCpy);
-	return "to monke";
+	return out;
 }
 
 #ifndef __PROGTEST__
@@ -164,15 +206,34 @@ int main ( int argc, char * argv [] )
     { NULL, NULL }
   };
   
+  const char * d3 [][2] = 
+  {
+    { "failure", "non-traditional success" },
+    { "fail", "suboptimal result" },
+    { NULL, NULL }
+  };
+  /*
+  const char * tbl [][2] =
+  {
+    { "son", "offspring with both X and Y chromosomes" },
+    { "prisoner", "client of the correctional system" },
+    { NULL, NULL }
+  };
+
+  res = newSpeak("son of a prisoner", tbl);
+  assert (!strcmp(res, "offspring with both X and Y chromosomes of a client of the correctional system"));
+  free(res);
+*/
+  res = newSpeak ( "Hello.", d2 );
+  assert ( ! res );
+  
+  res = newSpeak("Hello.", d3);
+  assert(!res);
+  
   res = newSpeak ( "Everybody is happy.", d1 );
   assert ( ! strcmp ( res, "Everybody is happy." ) );
   free ( res );
   
-  /*
-  res = newSpeak("murderer murderer, murderered", d1);
-  assert ( ! strcmp (res, "termination specialist termination specialist, termination specialisted"));
-  free(res);
-  */
   res = newSpeak ( "The student answered an incorrect answer.", d1 );
   assert ( ! strcmp ( res, "The client answered an alternative answer." ) );
   free ( res );
@@ -188,10 +249,6 @@ int main ( int argc, char * argv [] )
   res = newSpeak ( "Devil's advocate.", d1 );
   assert ( ! strcmp ( res, "Dnicenest deprived's advocate." ) );
   free ( res );
-
-
-  res = newSpeak ( "Hello.", d2 );
-  assert ( ! res );
 
   return EXIT_SUCCESS;
 }
